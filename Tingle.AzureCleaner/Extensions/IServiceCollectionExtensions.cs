376f617a95b1660855c3c956fa55c63dd1d6dfaf
Maxwell Weru <mburumaxwell@gmail.com>
2024-03-08 12:43:45 +0300

    Add CLI functionality to allow invoking via docker

---
+++ Tingle.AzureCleaner/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,48 @@
+﻿using Azure.Identity;
+using Tingle.AzureCleaner;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+internal enum EventBusTransportKind { InMemory, ServiceBus, QueueStorage, }
+
+internal static class IServiceCollectionExtensions
+{
+    public static IServiceCollection AddCleaner(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddMemoryCache();
+        services.Configure<AzureCleanerOptions>(configuration);
+        services.AddSingleton<AzureCleaner>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddEventBus(this IServiceCollection services, IConfiguration configuration, Action<EventBusBuilder>? setupAction = null)
+    {
+        var selectedTransport = configuration.GetValue<EventBusTransportKind?>("EventBus:SelectedTransport");
+        services.AddEventBus(builder =>
+        {
+            // Setup consumers
+            builder.AddConsumer<ProcessAzdoCleanupEventConsumer>();
+
+            // Setup transports
+            var credential = new DefaultAzureCredential();
+            if (selectedTransport is EventBusTransportKind.ServiceBus)
+            {
+                builder.AddAzureServiceBusTransport(
+                    options => ((AzureServiceBusTransportCredentials)options.Credentials).TokenCredential = credential);
+            }
+            else if (selectedTransport is EventBusTransportKind.QueueStorage)
+            {
+                builder.AddAzureQueueStorageTransport(
+                    options => ((AzureQueueStorageTransportCredentials)options.Credentials).TokenCredential = credential);
+            }
+            else if (selectedTransport is EventBusTransportKind.InMemory)
+            {
+                builder.AddInMemoryTransport();
+            }
+
+            setupAction?.Invoke(builder);
+        });
+        return services;
+    }
+}
